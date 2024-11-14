5beca9db6039d1820a4cc1a5a0f6d9a863edce60
coderJakub <kliemann.jakub@gmail.com>
2024-11-15 00:26:56 +0100

    timer styled

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -2,8 +2,10 @@
   <div class="d-flex flex-column p-4 justify-content-between page">
     <h1>Timer</h1>
     
-    <div class="d-flex justify-content-center d-flex align-items-center h-75">
-      <div class="rounded-circle bg-light h-50 timer"></div>
+    <div class="d-flex justify-content-center align-items-center h-75" style="position: relative;">
+        <div class="rounded-circle bg-light h-50 timer"></div>
+        <div class="time text-primary">2:30</div>
+        <div class="test" style="height: 55%; --percentage: 30;"></div>
     </div>
     <div>
       <div value='20' class="progress">
@@ -17,9 +19,22 @@
 <style scoped>
   .timer {
     aspect-ratio: 1;
+    z-index: 3;
   }
   .page{
     height: 92vh;
   }
-
+  .test{
+    aspect-ratio: 1;
+    border-radius: 100%;
+    position: absolute;
+    z-index: 1;
+    background: conic-gradient(#007bff calc(var(--percentage) * 1%), lightgray 0);
+  }
+  .time{
+    z-index: 4;
+    font-weight: bold;
+    font-size: 2rem;
+    position: absolute;
+  }
 </style>
