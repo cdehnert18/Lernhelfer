06a46767e39c1ee2f934b1929f7d8458e83e72e8
coderJakub <kliemann.jakub@gmail.com>
2024-11-14 19:56:58 +0100

    set html items

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <h1 v-for="n in 30" :key="n">Timer {{ n }}</h1>
+  <div class="d-flex flex-column p-4 justify-content-between page">
+    <h1>Timer</h1>
+    
+    <div class="d-flex justify-content-center d-flex align-items-center h-75">
+      <div class="rounded-circle bg-light h-50 timer"></div>
+    </div>
+    <div>
+      <div value='20' class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuemin="0" aria-valuemax="100">25%</div>
+      </div>
+    </div>
   </div>
+
 </template>
+
+<style scoped>
+  .timer {
+    aspect-ratio: 1;
+  }
+  .page{
+    height: 92vh;
+  }
+
+</style>
