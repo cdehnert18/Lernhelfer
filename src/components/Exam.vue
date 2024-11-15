285eb780874f9470b8b84d274217b9c44982af9e
cdehnert18 <clemensdehnert@gmail.com>
2024-11-15 16:24:39 +0100

    - Exam & ExamDay hinzugefügt

---
+++ src/components/Exam.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'ExamComponent',
+})
+
+const props = defineProps<{
+  pruefung: string
+  zeitraumStart: Date
+  zeitraumEnde: Date
+}>()
+
+const zeitraum = computed(() => {
+  const hoursStart = props.zeitraumStart.getHours().toString().padStart(2, '0');
+  const minutesStart = props.zeitraumStart.getMinutes().toString().padStart(2, '0');
+  const hoursEnde = props.zeitraumEnde.getHours().toString().padStart(2, '0');
+  const minutesEnde = props.zeitraumEnde.getMinutes().toString().padStart(2, '0');
+  const zeitraum = `${hoursStart}:${minutesStart} - ${hoursEnde}:${minutesEnde}`;
+
+  return zeitraum
+})
+</script>
+
+<template>
+  <div
+    class="container border border-tertiary bg-body-secondary rounded shadow p-3"
+  >
+    <div class="row align-items-center">
+      <div class="col">
+        <h6>{{ pruefung }}</h6>
+        <small>{{ zeitraum }}</small>
+      </div>
+      <div class="col-auto d-flex gap-2">
+        <button class="btn btn-danger">Löschen</button>
+        <router-link to="/" class="btn btn-success">Bearbeiten</router-link>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/PruefungView.vue
+++ src/views/PruefungView.vue
@@ -1,3 +1,56 @@
+<script setup lang="ts">
+import ExamDay from '@/components/ExamDay.vue'
+
+const rawPruefungen = [
+  {
+    fach: 'Geschichte 1',
+    start: new Date('2024-12-03T16:00:00'),
+    ende: new Date('2024-12-03T17:30:00'),
+  },
+  {
+    fach: 'Geschichte 2',
+    start: new Date('2024-12-03T19:00:00'),
+    ende: new Date('2024-12-03T21:30:00'),
+  },
+  {
+    fach: 'Mathematik',
+    start: new Date('2024-12-05T10:45:00'),
+    ende: new Date('2024-12-05T11:30:00'),
+  },
+  {
+    fach: 'Literatur',
+    start: new Date('2024-12-15T14:00:00'),
+    ende: new Date('2024-12-15T15:30:00'),
+  },
+]
+
+const groupedPruefungen = rawPruefungen.reduce((acc, pruefung) => {
+  // Extrahiere das Datum (ohne Uhrzeit) als Date-Objekt
+  const datum = new Date(pruefung.start.toISOString().split('T')[0])  // '2024-12-03' als Date-Objekt
+
+  // Suche nach einem bestehenden Datumseintrag (Vergleich mit Date)
+  const existingEntry = acc.find((entry) => entry.datum.getTime() === datum.getTime())  // Vergleiche mit .getTime()
+
+  if (existingEntry) {
+    // Datum existiert bereits: Füge die Prüfung hinzu
+    existingEntry.pruefungen.push(pruefung)
+  } else {
+    // Neues Datum: Erstelle einen neuen Eintrag
+    acc.push({
+      datum: datum,  // datum als Date
+      pruefungen: [pruefung],
+    })
+  }
+
+  return acc
+}, [] as { datum: Date; pruefungen: typeof rawPruefungen }[])  // Datum als Date im Accumulator
+</script>
+
 <template>
-  <h1>Prüfung</h1>
+  <ExamDay
+    v-for="(pruefungstag, index) in groupedPruefungen"
+    :key="index"
+    :datum="pruefungstag.datum"
+    :pruefungen="pruefungstag.pruefungen"
+  />
 </template>
