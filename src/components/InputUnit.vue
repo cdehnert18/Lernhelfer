20b68a4faa01f9e42ea7513133ebbc0268e3c3a4
flo6669 <147403414+flo6669@users.noreply.github.com>
2024-11-16 22:55:57 +0100

    added hinzufuegen view basics

---
+++ src/components/InputUnit.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { ref } from 'vue';
+
+defineProps<{
+    label: string;
+    inputType: string;
+}>();
+
+const inputValue = ref('');
+</script>
+
+<template>
+    <label for="inputField">{{ label }}</label>
+    <input id="inputField" v-model="inputValue" :type="inputType" />
+</template>
--- src/views/HinzufuegenView.vue
+++ src/views/HinzufuegenView.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+    import InputUnit from '@/components/InputUnit.vue'
+    const options = [{text: 'leicht'}, {text: 'mittel'}, {text: 'schwer'}]
+</script>
+
 <template>
-  <h1>Hinzufügen</h1>
+  <InputUnit label="Name" inputType="text"/>
+  <InputUnit label="Datum" inputType="date"/>
+  <InputUnit label="Arbeitsaufwand" inputType ="number"/>
+  <InputUnit label="Lernbeginn" inputType="date"/>
+  <select id="selectDifficulty">
+    <option v-for="(option, index) in options" :key="index">
+      {{ option.text }}
+    </option>
+  </select>
+  <button class="btn">Abbrechen</button>
+  <button class="btn btn-primary">Speichern</button>
 </template>
