dd2d4b5718ba3a954a6bdd29f343b42249271259
flo6669 <147403414+flo6669@users.noreply.github.com>
2024-11-18 17:54:35 +0100

    added hinzufuegen view
    * added hinzufuegen view basics
    
    * added some styling to hinzufuegen view
    
    * changed styling of hinzufuegen view
    
    * added time to hinzufuegen view

---
+++ src/components/InputUnit.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { ref } from 'vue';
+
+defineProps<{
+    label: string;
+    inputType: string;
+    placeholder: string;
+}>();
+
+const inputValue = ref('');
+</script>
+
+<template>
+    <div class="d-flex flex-column" style="width: 80%;">
+        <label for="inputField" class="form-label">{{ label }}</label>
+        <input id="inputField"  class="form-control" v-model="inputValue" :type="inputType" :placeholder="placeholder" />
+    </div>
+</template>
--- src/views/HinzufuegenView.vue
+++ src/views/HinzufuegenView.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import InputUnit from '@/components/InputUnit.vue'
+const options = [{ text: 'leicht' }, { text: 'mittel' }, { text: 'schwer' }]
+const inputUnits = [
+  { label: 'Prüfungsname', inputType: 'text', placeholder: 'z.B. Mathematik 1' },
+  { label: 'Prüfungsdatum', inputType: 'date', placeholder: '' },
+  { label: 'Uhrzeit', inputType: 'time', placeholder: '' },
+  { label: 'Arbeitsaufwand', inputType: 'number', placeholder: 'z.B. 20' },
+  { label: 'Lernbeginn', inputType: 'date', placeholder: '' }
+]
+</script>
+
 <template>
-  <h1>Hinzufügen</h1>
+  <div class="d-flex flex-column justify-content-between align-items-center pt-4" style="height: 88vh;">
+    <InputUnit v-for="(unit, index) in inputUnits" :key=index :label=unit.label :inputType=unit.inputType
+      :placeholder=unit.placeholder />
+    <div class="d-flex flex-column" style="width: 80%;">
+      <label for="selectDifficulty" class="form-label">Schwierigkeitsgrad</label>
+      <select id="selectDifficulty" class="form-select">
+        <option v-for="(option, index) in options" :key="index">
+          {{ option.text }}
+        </option>
+      </select>
+    </div>
+    <div class="d-flex justify-content-between">
+      <button class="btn">Abbrechen</button>
+      <button class="btn btn-primary">Speichern</button>
+    </div>
+  </div>
 </template>
+
+<style scoped></style>
