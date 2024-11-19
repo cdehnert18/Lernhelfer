2aa1e5f4820e428eb44225178fb86fef93434dff
Clemens Dehnert <clemensdehnert@gmail.com>
2024-11-19 15:59:45 +0100

    - remove unnecessary import

--- src/components/InputUnit.vue
+++ src/components/InputUnit.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 defineProps<{
-    label: string;
-    inputType: string;
-    placeholder: string;
-}>();
+  label: string
+  inputType: string
+  placeholder: string
+}>()
 
-const inputValue = ref('');
+const inputValue = ref('')
 </script>
 
 <template>
-    <div class="d-flex flex-column" style="width: 80%;">
-        <label for="inputField" class="form-label">{{ label }}</label>
-        <input id="inputField"  class="form-control" v-model="inputValue" :type="inputType" :placeholder="placeholder" />
-    </div>
+  <div class="d-flex flex-column" style="width: 80%">
+    <label for="inputField" class="form-label">{{ label }}</label>
+    <input
+      id="inputField"
+      class="form-control"
+      v-model="inputValue"
+      :type="inputType"
+      :placeholder="placeholder"
+    />
+  </div>
 </template>
