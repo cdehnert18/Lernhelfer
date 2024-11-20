6e431ebbbd6af7e346d61e18c7278ba3e69947dc
Clemens Dehnert <clemensdehnert@gmail.com>
2024-11-20 18:35:47 +0100

    - simplified code

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@ import Navigation from '@/components/Navigation.vue'
 
 <template>
   <div class="d-flex flex-column min-vh-100">
-    <div class="flex-grow-1">
-      <RouterView class="flex-grow-1" />
-    </div>
+    <RouterView class="d-flex flex-column flex-grow-1" />
     <Navigation class="sticky-bottom bg-secondary-subtle" />
   </div>
 </template>
