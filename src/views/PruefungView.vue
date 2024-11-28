c90b3ed87aa4113c9815af396d42ac0b56a1fd05
Clemens Dehnert <clemensdehnert@gmail.com>
2024-11-28 17:34:50 +0100

    remove onMounted

--- src/views/PruefungView.vue
+++ src/views/PruefungView.vue
@@ -1,60 +1,59 @@
 <script lang="ts">
-import { defineComponent, computed, onMounted } from 'vue';
-import { usePruefungenStore } from '@/stores/Exam';
-import ExamDay from '@/components/ExamDay.vue';
+import { defineComponent, computed } from 'vue'
+import { usePruefungenStore } from '@/stores/Exam'
+import ExamDay from '@/components/ExamDay.vue'
 
 export default defineComponent({
   components: {
     ExamDay,
   },
   setup() {
-    const store = usePruefungenStore();
+    const store = usePruefungenStore()
 
     // Gruppiere die Prüfungen nach Datum
     const groupedPruefungen = computed(() => {
       return store.pruefungen.reduce(
         (acc, pruefung) => {
-          const datum = new Date(pruefung.date.toISOString().split('T')[0]);
-          
+          const datum = new Date(pruefung.date.toISOString().split('T')[0])
+
           const existingEntry = acc.find(
-            (entry) => entry.datum.getTime() === datum.getTime()
-          );
+            entry => entry.datum.getTime() === datum.getTime(),
+          )
 
           if (existingEntry) {
-            existingEntry.pruefungen.push(pruefung);
+            existingEntry.pruefungen.push(pruefung)
           } else {
             acc.push({
               datum: datum,
               pruefungen: [pruefung],
-            });
+            })
           }
 
-          return acc;
+          return acc
         },
         [] as { datum: Date; pruefungen: typeof store.pruefungen }[],
-      );
-    });
+      )
+    })
 
     // Löschen einer Prüfung
     const handleExamDeleted = (fach: string, date: Date) => {
       const index = store.pruefungen.findIndex(
-        (pruefung) =>
-          pruefung.fach === fach && pruefung.date.getTime() === date.getTime()
-      );
+        pruefung =>
+          pruefung.fach === fach && pruefung.date.getTime() === date.getTime(),
+      )
 
       if (index > -1) {
         store.removePruefung(index) // Methode aus dem Store nutzen
       }
-    };
+    }
 
     return {
       groupedPruefungen,
       handleExamDeleted,
-    };
+    }
   },
-});
+})
 </script>
-
 
 <template>
   <div>
