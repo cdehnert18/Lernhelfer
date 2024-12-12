bb56d35ae17de0007e0c33eea77d355e2443e8a2
Clemens Dehnert <clemensdehnert@gmail.com>
2024-12-12 23:50:04 +0100

    refactored datetime-local to date & time

--- src/stores/Exam.ts
+++ src/stores/Exam.ts
@@ -27,24 +27,26 @@ export const usePruefungenStore = defineStore('pruefungen', () => {
     const data = localStorage.getItem('pruefungen')
     pruefungen.value = data
       ? JSON.parse(data).map(
-        (item: {
-          name: string
-          examDate: string
-          effort: number
-          start: string
-          difficulty: string
-          buffer: number
-          excludedDays: string[]
-        }) => ({
-          name: item.name,
-          examDate: new Date(item.examDate),
-          effort: item.effort,
-          start: new Date(item.start),
-          difficulty: item.difficulty,
-          buffer: item.buffer,
-          excludedDays: item.excludedDays.map((date: string) => new Date(date)),
-        }),
-      )
+          (item: {
+            name: string
+            examDate: string
+            workload: number
+            start: string
+            difficulty: string
+            buffer: number
+            excludedDays: string[]
+          }) => ({
+            name: item.name,
+            examDate: new Date(item.examDate),
+            workload: item.workload,
+            start: new Date(item.start),
+            difficulty: item.difficulty,
+            buffer: item.buffer,
+            excludedDays: item.excludedDays.map(
+              (date: string) => new Date(date),
+            ),
+          }),
+        )
       : []
   }
 
@@ -55,7 +57,9 @@ export const usePruefungenStore = defineStore('pruefungen', () => {
     // algorithmus erzeugt array von learnunits (newLearunits) (learnunit = {exam: pruefung(parameter aus Funktionskopf), date: Date, duration: number, done: false})
     // dann newLearunits.forEach(learnunit => useLearnUnitStore().addLearnunit(learnunit)) speichert alle learnunits in Pinia
     const newLearnUnits: Learnunit[] = initLearnPlan(pruefung)
-    newLearnUnits.forEach((learnunit) => useLearnUnitStore().addLearnunit(learnunit));
+    newLearnUnits.forEach(learnunit =>
+      useLearnUnitStore().addLearnunit(learnunit),
+    )
   }
 
   const removePruefung = (index: number) => {
@@ -78,40 +82,44 @@ const initLearnPlan = (pruefung: Pruefung) => {
   } else {
     pruefung.buffer = 6
   }
-  const newLearnunits = createLearnPlan(pruefung);
+  const newLearnunits = createLearnPlan(pruefung)
   return newLearnunits
-};
+}
 
 const createLearnPlan = (pruefung: Pruefung) => {
-  let amountDays;
-  let actualStartDate;
+  let amountDays
+  let actualStartDate
   if (pruefung.start.getTime() > Date.now()) {
-    amountDays = (pruefung.examDate.getTime() - pruefung.start.getTime()) / (1000 * 60 * 60 * 24) - pruefung.excludedDays.length;
+    amountDays =
+      (pruefung.examDate.getTime() - pruefung.start.getTime()) /
+        (1000 * 60 * 60 * 24) -
+      pruefung.excludedDays.length
     actualStartDate = pruefung.start
-  }
-  else {
-    amountDays = (pruefung.examDate.getTime() - Date.now()) / (1000 * 60 * 60 * 24) - pruefung.excludedDays.length;
+  } else {
+    amountDays =
+      (pruefung.examDate.getTime() - Date.now()) / (1000 * 60 * 60 * 24) -
+      pruefung.excludedDays.length
     actualStartDate = new Date(Date.now())
   }
-  if (pruefung.examDate.getHours() > 12)
-    amountDays++
+  if (pruefung.examDate.getHours() > 12) amountDays++
   const learningTime = Math.round((pruefung.workload / amountDays) * 60)
   const newLearnunits = []
   const currentDateIterator = actualStartDate
 
   while (currentDateIterator <= pruefung.examDate) {
     const isExcludedDay = pruefung.excludedDays.some(
-      (excludedDate) => new Date(excludedDate).toDateString() === currentDateIterator.toDateString()
-    );
+      excludedDate =>
+        new Date(excludedDate).toDateString() ===
+        currentDateIterator.toDateString(),
+    )
     if (!isExcludedDay) {
       newLearnunits.push({
         exam: pruefung,
         date: new Date(currentDateIterator),
         duration: learningTime,
-      });
+      })
     }
-    currentDateIterator.setDate(currentDateIterator.getDate() + 1);
+    currentDateIterator.setDate(currentDateIterator.getDate() + 1)
   }
-  return newLearnunits;
-};
-
+  return newLearnunits
+}
